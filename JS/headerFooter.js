e930a560f870548a39527df5075a11d1a482b358
Amani Vital Ruivo <amanivital23@gmail.com>
2026-07-29 21:39:12 -0300

    arrumando js header e footer

--- JS/headerFooter.js
+++ JS/headerFooter.js
@@ -1,16 +1,22 @@
-const login = localStorage.getItem('login') === 'true';
+const login = localStorage.getItem("login") === "true";
 
 async function carregar(arquivo, id) {
-  const busca = await fetch(arquivo);
-  const dados = await busca.text();
-  document.getElementById(id).innerHTML = dados;
+    const res = await fetch(arquivo);
+    const html = await res.text();
+    document.getElementById(id).innerHTML = html;
 }
 
 if (login) {
- carregar('../componentes/headerLogado.html', 'headerLogado');
-  carregar('../componentes/footerLogado.html', 'footerLogado');
+    carregar("../componentes/headerLogado.html", "header");
+    carregar("../componentes/footerLogado.html", "footer");
+
+    document.getElementById("css-header").href = "../componentes/headerLogado.css";
+    document.getElementById("css-footer").href = "../componentes/footerLogado.css";
+
 } else {
-  
-  carregar('../componentes/header.html', 'header');
-  carregar('../componentes/footer.html', 'footer');
+    carregar("../componentes/header.html", "header");
+    carregar("../componentes/footer.html", "footer");
+
+    document.getElementById("css-header").href = "../componentes/header.css";
+    document.getElementById("css-footer").href = "../componentes/footer.css";
 }
