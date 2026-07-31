e762ad6b93248fb8ede9a54472cac9a58f1ed7b2
Amani Vital Ruivo <amanivital23@gmail.com>
2026-07-31 17:24:43 -0300

    fazendo alteracoes

--- JS/headerFooter.js
+++ JS/headerFooter.js
@@ -6,7 +6,9 @@ async function carregar(arquivo, id) {
     document.getElementById(id).innerHTML = html;
 }
 
+
 if (login) {
+
     carregar("../componentes/headerLogado.html", "header");
     carregar("../componentes/footerLogado.html", "footer");
 
@@ -14,9 +16,48 @@ if (login) {
     document.getElementById("css-footer").href = "../componentes/footerLogado.css";
 
 } else {
+
     carregar("../componentes/header.html", "header");
     carregar("../componentes/footer.html", "footer");
 
     document.getElementById("css-header").href = "../componentes/header.css";
     document.getElementById("css-footer").href = "../componentes/footer.css";
+}
+
+
+function sair() {
+    localStorage.removeItem("login");
+    window.location.reload();
+}
+
+
+function alterarSenha() {
+
+    const novaSenha = prompt("Digite sua nova senha:");
+
+    if (novaSenha) {
+
+        localStorage.setItem("senha", novaSenha);
+
+        alert("Senha alterada com sucesso!");
+
+    }
+
+}
+
+
+function excluirConta() {
+
+    const confirmar = confirm("Deseja realmente excluir sua conta?");
+
+    if(confirmar){
+
+        localStorage.clear();
+
+        alert("Conta excluída!");
+
+        window.location.href = "../index.html";
+
+    }
+
 }
