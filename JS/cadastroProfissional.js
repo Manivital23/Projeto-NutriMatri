242f40fbdb14284613331f542f56c2c8d2d9362b
JuNeves-Bts <anajulia15e.18@gmail.com>
2026-08-13 19:19:54 -0300

    subito cadastro profissional

---
+++ JS/cadastroProfissional.js
@@ -0,0 +1,13 @@
+const botao = document.getElementById('btn-Info')
+const modal = document.getElementById("moda-lInfo")
+const close = document.getElementById("fechar")
+
+botao.addEventListener("click", (e)=> {
+  e.preventDefault()
+  modal.classList.add("ativo")
+})
+
+close.addEventListener("click", (e)=> {
+e.preventDefault()
+modal.classList.remove("ativo")
+})
