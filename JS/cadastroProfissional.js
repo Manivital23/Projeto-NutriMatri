afef09a98a14cf37069005cc6d0da785d4d73b52
Ana Julia <ajuliaribeirorocha@gmail.com>
2026-08-13 21:16:30 -0300

    Ajuste de caminhos nos arquivos V6

--- JS/cadastroProfissional.js
+++ JS/cadastroProfissional.js
@@ -1,11 +1,6 @@
 const botao = document.getElementById('btn-Info')
 const modal = document.getElementById("moda-lInfo")
 const close = document.getElementById("fechar")
-const btnEnviar = document.querySelector("btn-eviar1")
-
-btnEnviar.addEventListener("click",()=>{
-  window.location.href = "/index.html"
-})
 
 botao.addEventListener("click", (e)=> {
   e.preventDefault()
@@ -13,6 +8,6 @@ botao.addEventListener("click", (e)=> {
 })
 
 close.addEventListener("click", (e)=> {
-e.preventDefault()
-modal.classList.remove("ativo")
+  e.preventDefault()
+  modal.classList.remove("ativo")
 })
